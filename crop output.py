f5519da9cb9d3288d07780194a5d63325ac24b31
Supriya Mourya <supriyamaurya2576@gmail.com>
2025-11-22 15:43:14 +0530

    Add files via upload

---
+++ crop output.py
@@ -0,0 +1,45 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============= RESTART: C:/Users/supriya/OneDrive/Documents/crop.py =============
+ TO HEIP FARMER TO UNDERSTAND IN CROP GROWING
+Enter your city :mumbai
+Selecting the sutiable crop according to your city
+Climate:humid  ,dry season
+Sutiable: root vegetable, pepper
+you can choose from: ['root vegetable', 'pepper']
+Enter the crop name:pepper
+Crop requirement
+Soil: Loamy soil
+Temp: 18-30°C
+Rainfall: 600-900 mm
+>>> 
+============= RESTART: C:/Users/supriya/OneDrive/Documents/crop.py =============
+ TO HEIP FARMER TO UNDERSTAND IN CROP GROWING
+Enter your city :varansi
+Selecting the sutiable crop according to your city
+City not found. Showing general crops.
+ Your city will enter soon.
+you can choose from: ['paddy', 'wheat']
+Enter the crop name:wheat
+Crop requirement
+Soil: Loam to clay loam
+Temp: 10-25°C
+Rainfall: 450-650 mm
+Season: Rabi
+Growth: 100-140 days
+>>> 
+============= RESTART: C:/Users/supriya/OneDrive/Documents/crop.py =============
+ TO HEIP FARMER TO UNDERSTAND IN CROP GROWING
+Enter your city :kanpur
+Selecting the sutiable crop according to your city
+City not found. Showing general crops.
+ Your city will enter soon.
+you can choose from: ['paddy', 'wheat']
+Enter the crop name:wheat
+Crop requirement
+Soil: Loam to clay loam
+Temp: 10-25°C
+Rainfall: 450-650 mm
+Season: Rabi
+Growth: 100-140 days
